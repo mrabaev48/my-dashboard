f4a8ee321ad4522cdd03c8ddd486c75106cb5957
msoosaar <mikhail.soosaar@fizzdev.ee>
2022-01-21 16:16:01 +0200

    refactor & cleanup

--- src/components/shared/dataTables/dataTablesComponents/dataTablesBodyRow/DataTablesBodyRow.tsx
+++ src/components/shared/dataTables/dataTablesComponents/dataTablesBodyRow/DataTablesBodyRow.tsx
@@ -1,33 +1,11 @@
 import React, {CSSProperties, FC} from "react";
 import {useDataTablesContext} from "../../config/hooks/useDataTablesContext";
-import {DataTablesStringCell} from "../dataTablesCell/DataTablesStringCell";
-import {DataTablesIntCell} from "../dataTablesCell/DataTablesIntCell";
-import {DataTablesBooleanCell} from "../dataTablesCell/DataTablesBooleanCell";
-import {DataTablesCurrencyCell} from "../dataTablesCell/DataTablesCurrencyCell";
-import {DataTablesSelectCell} from "../dataTablesCell/DataTablesSelectCell";
-import {DataTablesDecimalCell} from "../dataTablesCell/DataTablesDecimalCell";
-import {DataTablesActionCell} from "../dataTablesCell/DataTablesActionCell";
-import {DataTablesDateCell} from "../dataTablesCell/DataTablesDateCell";
-import {TableCell, TableRow} from "@mui/material";
+import {TableRow} from "@mui/material";
 
 import _ from 'lodash';
 
-import {DataTablesSelectionCell} from "../dataTablesCell/DataTablesSelectionCell";
-import {DataTablesExpandCell} from "../dataTablesCell/DataTablesExpandCell";
-import {DataTablesColumnType} from "../../models/DataTablesColumnType";
+import {DataTablesBodyCell} from "../dataTablesCell/DataTablesBodyCell";
 
-const TABLE_COLUMNS: any = {
-    SELECTION: DataTablesSelectionCell,
-    STRING: DataTablesStringCell,
-    INT: DataTablesIntCell,
-    DECIMAL: DataTablesDecimalCell,
-    BOOLEAN: DataTablesBooleanCell,
-    SELECT: DataTablesSelectCell,
-    CURRENCY: DataTablesCurrencyCell,
-    ACTION: DataTablesActionCell,
-    DATE: DataTablesDateCell,
-    EXPAND: DataTablesExpandCell,
-}
 
 export interface IDataTablesBodyRowProps {
     rowCells: any;
@@ -35,46 +13,21 @@ export interface IDataTablesBodyRowProps {
     style?: CSSProperties;
 }
 
-export const DataTablesBodyRow:FC<IDataTablesBodyRowProps> = ({rowCells, className, style}) => {
+export const DataTablesBodyRow: FC<IDataTablesBodyRowProps> = ({rowCells, className, style}) => {
     const context = useDataTablesContext();
 
-    const getPropertiesByColumnType = (type: DataTablesColumnType) => {
-        switch (type) {
-            case DataTablesColumnType.EXPAND:
-                return {
-                    className: 'dt-expand-column',
-                    style
-                };
-            case DataTablesColumnType.SELECTION:
-                return {
-                    className: 'dt-selection-column',
-                    style: {}
-                };
-            default:
-                return {
-                    className: '',
-                    style: {}
-                };
-        }
-    }
-
     const cells = context.options.columns.map((column, index) => {
-        const ColumnComponent = TABLE_COLUMNS[column.type];
-        const errorMessage = false;
-        const properties = getPropertiesByColumnType(column.type);
+
+
         return (
-            <TableCell
+            <DataTablesBodyCell
                 key={column.dataSource + '_' + index}
-                className={properties.className}
-            >
-                <ColumnComponent
-                    useAutofocus={index === 0}
-                    cellValue={_.get(rowCells, column.dataSource)}
-                    column={column}
-                    rowCells={rowCells}
-                    style={properties.style}
-                />
-            </TableCell>
+                column={column}
+                row={rowCells}
+                value={_.get(rowCells, column.dataSource)}
+                useAutofocus={index === 0}
+                style={style}
+            />
         )
     });
 
---
+++ src/components/shared/dataTables/dataTablesComponents/dataTablesCell/DataTablesBodyCell.tsx
@@ -0,0 +1,82 @@
+import {CSSProperties, FC} from "react";
+import {DataTablesColumnType} from "../../models/DataTablesColumnType";
+import {DataTablesColumn, IDataTablesDateColumn, IDataTablesSelectColumn} from "../../models/IDataTablesColumn";
+import {DataTablesStringCell} from "./DataTablesStringCell";
+import {DataTablesIntCell} from "./DataTablesIntCell";
+import {DataTablesDecimalCell} from "./DataTablesDecimalCell";
+import {DataTablesBooleanCell} from "./DataTablesBooleanCell";
+import {DataTablesSelectCell} from "./DataTablesSelectCell";
+import {DataTablesCurrencyCell} from "./DataTablesCurrencyCell";
+import {DataTablesActionCell} from "./DataTablesActionCell";
+import {DataTablesDateCell} from "./DataTablesDateCell";
+import {DataTablesSelectionCell} from "./DataTablesSelectionCell";
+import {DataTablesExpandCell} from "./DataTablesExpandCell";
+import {TableCell} from "@mui/material";
+import {DtUtils} from "../../utils/DtUtils";
+
+export interface IDataTablesBodyCellProps {
+    column: DataTablesColumn;
+    row: any;
+    value: any;
+    useAutofocus: boolean;
+    style?: CSSProperties;
+}
+
+export const DataTablesBodyCell: FC<IDataTablesBodyCellProps> = ({
+                                                                     column,
+                                                                     row,
+                                                                     value,
+                                                                     useAutofocus,
+                                                                     style
+                                                                 }) => {
+    let control = null;
+    let className = '';
+
+    switch (column.type) {
+        case DataTablesColumnType.STRING:
+            control = <DataTablesStringCell cellValue={value} useAutofocus={useAutofocus} column={column} rowCells={row}/>;
+            break;
+        case DataTablesColumnType.INT:
+            control = <DataTablesIntCell cellValue={value} useAutofocus={useAutofocus} column={column} rowCells={row}/>;
+            break;
+        case DataTablesColumnType.DECIMAL:
+            control = <DataTablesDecimalCell cellValue={value} useAutofocus={useAutofocus} column={column} rowCells={row}/>
+            break;
+        case DataTablesColumnType.BOOLEAN:
+            control = <DataTablesBooleanCell cellValue={value} useAutofocus={useAutofocus} column={column} rowCells={row}/>;
+            break;
+        case DataTablesColumnType.SELECT:
+            control = <DataTablesSelectCell cellValue={value} useAutofocus={useAutofocus}
+                                            column={column as IDataTablesSelectColumn} rowCells={row}/>;
+            break;
+        case DataTablesColumnType.CURRENCY:
+            control = <DataTablesCurrencyCell cellValue={value} useAutofocus={useAutofocus} column={column} rowCells={row}/>;
+            break;
+        case DataTablesColumnType.ACTION:
+            control = <DataTablesActionCell rowCells={row}/>;
+            break;
+        case DataTablesColumnType.DATE:
+            control = <DataTablesDateCell cellValue={value} useAutofocus={useAutofocus}
+                                          column={column as IDataTablesDateColumn} rowCells={row}/>;
+            break;
+        case DataTablesColumnType.SELECTION:
+            className = 'dt-selection-column';
+            control = <DataTablesSelectionCell rowCells={row}/>;
+            break;
+        case DataTablesColumnType.EXPAND:
+            className = 'dt-expand-column';
+            control = <DataTablesExpandCell rowCells={row} style={style}/>;
+            break;
+        case DataTablesColumnType.EMPTY:
+        default:
+           control = <></>;
+    }
+
+    return (
+        <TableCell
+            className={`${DtUtils.getCellClassNameByColumn(column)} ${className}`}
+        >
+            {control}
+        </TableCell>
+    )
+}
