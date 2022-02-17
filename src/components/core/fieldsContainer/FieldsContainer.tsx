fb4384bc0bc8a9d486be63f18617e61a341dc877
msoosaar <mikhail.soosaar@fizzdev.ee>
2022-02-17 13:50:01 +0200

    refactor

--- src/components/core/fieldsContainer/FieldsContainer.tsx
+++ src/components/core/fieldsContainer/FieldsContainer.tsx
@@ -2,8 +2,6 @@ import React, {FC} from 'react';
 import {Grid, Paper} from "@mui/material";
 import {IField} from "./models";
 import {FieldControl} from "../fields/FieldControl";
-import {FIELD_TYPE} from "../fields/models";
-import {KeyValuePair} from "../../shared/dataTables/utils/DtUtils";
 import {Permission} from "../models/Permission";
 
 export interface IFieldsContainerProps {
